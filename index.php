d0334a3c1a961a8bd59014ee6240a32268a39959
Villian <villian780@gmail.com>
2023-02-24 20:27:30 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<style>
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
+  </head>
+  <body>
+    <div class="container mt-3">
+      <div class="card mb-3 p-3">
+        <div class="card-body">
+          <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ex molestias, laborum iste qui rerum est amet architecto fugit ad odit sit, magni nobis facere. Autem sit incidunt, officiis natus facere eligendi. Veniam velit quas praesentium doloribus in inventore, optio numquam cum? Laboriosam, illo doloremque. Alias adipisci quam asperiores deserunt.
+          </p>
+          <div class="d-flex">
+            <button type="button" class="btn btn-primary ms-auto">Details</button>
+          </div>
+        </div>
+      </div>
+      <div class="card p-3">
+        <div class="card-body">
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi esse laborum, voluptas veritatis aliquid, ut exercitationem adipisci, maxime modi quibusdam quasi! Natus ratione aliquam vel fugit atque! Doloremque alias assumenda commodi nemo explicabo quis, expedita necessitatibus vero hic odit veniam architecto obcaecati sequi perspiciatis consectetur corrupti? Magnam qui distinctio esse.
+          </p>
+          <div class="d-flex">
+            <button type="button" class="btn btn-primary ms-auto">Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
